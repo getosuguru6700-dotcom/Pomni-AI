247f7d6ece20493445f9e3ea9b3a6003759d1dd5
getosuguru6700-dotcom <getosuguru6700@gmail.com>
2026-05-11 04:50:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '2181550342', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
   // Owner 1
-    { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
+    { name: "kokushibu", lid: "247579682029763@lid", jid: "218921550342@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
+    { name: "geto", lid: "221307316789354@lid", jid: "201103345351@s.whatsapp.net" },
   // Owner 3
-    { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
-  // Owner 4 
-   { name: "عمورتي", jid: "201050079089@s.whatsapp.net", lid: "51664513925368@lid" }
+    { name: "mk", jid: "16838886639@s.whatsapp.net", lid: "50414477168824@lid" },
   ],
   settings: { noWelcome: false },
   commandsPath: './plugins'
@@ -33,22 +31,20 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = { 
-  nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
+  nameBot: " 𝙂𝙚𝙩𝙤 𝘽𝙤𝙩〈", 
   idChannel: "120363225356834044@newsletter",
   urls: {
     repo: "https://github.com/deveni0/Pomni-AI",
     api: "https://emam-api.web.id",
-    channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
+    channel: 
   },
   copyright: { 
     pack: 'ڤـ ـ VA ـ ـا', 
-    author: 'VA'
+    author'VA'
   },
   images: [
     "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
-    "https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg",
-    "https://i.pinimg.com/originals/bb/77/0f/bb770fad66a634a6b3bf93e9c00bf4e5.jpg"
+    "https://i.pinimg.com/originals/e2/21/20/epinimg.com/originals/bb/77/0f/bb770fad66a634a6b3bf93e9c00bf4e5.jpg"
   ]
 };
 
